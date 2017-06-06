dfacbe01a3c56b81fec47adc957e34c6911d6e09
ftuckettjr <ftuckettjr@gmail.com>
2017-06-05 23:22:23 -0400

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scaffolding8.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public Decimal? UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Models/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Scaffolding8.Models
+{
+    public class StoreContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+    }
+}
